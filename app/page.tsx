909a09ef324bdde665b8a74e4867a5dab9c32552
Zsuzsa Lukacs <zsuzsa@climatefarmers.org>
2024-06-27 16:58:47 +0200

    start one page and fix tailwind

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,20 @@
-
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-  <h1>It’s time to transform farming.</h1>
-<h3>What farmers have say</h3>
+    <main className="">
+      <section className="text-white bg-[url(/images/start_header.jpg)] bg-contain h-[600px] bg-no-repeat p-16 flex justify-center flex-col">
+        <h1>It’s time to transform farming.</h1>
+        <h4 className="my-8">
+          Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
+          nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat,
+          sed diam voluptua. At vero eos et accusam et justo duo dolores et ea
+          rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem
+          ipsum dolor sit amet.
+        </h4>
+        <button className="bg-lime self-start">Learn more</button>
+      </section>
+      <section>
+        <h3>What farmers have say</h3>
+      </section>
     </main>
   );
 }
