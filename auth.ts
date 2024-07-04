931382fe438ad289f29aff90772538aba69f940a
Zsuzsa Lukacs <zsuzsa@climatefarmers.org>
2024-07-04 08:49:24 +0200

    login with credentials

--- auth.ts
+++ auth.ts
@@ -1,9 +1,28 @@
-import type { NextAuthConfig } from "next-auth"
-import NextAuth from "next-auth"
-import Google from "next-auth/providers/google"
+import type { NextAuthConfig } from "next-auth";
+import NextAuth from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
+import Google from "next-auth/providers/google";
+
+const credentialsConfig =CredentialsProvider({
+name:"Credentials", 
+credentials: {
+email:{
+label:"Email",
+type: "email"},
+password: {
+label:"Password",
+type: "password"}
+},
+async authorize(credentials){
+if(credentials.email= "test@test.org" && credentials.password === "123") //replace with real database check
+return {
+name: "Test User" // in real word senario user object
+}
+else return null
+}})
  
 export const config=  {
-  providers: [Google],
+  providers: [Google, credentialsConfig],
 } satisfies NextAuthConfig
 
 export const {handlers, auth, signIn, signOut}=NextAuth(config)
