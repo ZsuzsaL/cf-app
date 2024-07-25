27c34621933a32c1fc79706a3947daf65f3618c7
Zsuzsa Lukacs <zsuzsa@climatefarmers.org>
2024-07-25 15:54:14 +0200

    fix save farm route

--- app/api/saveFarmInfo/route.ts
+++ app/api/saveFarmInfo/route.ts
@@ -2,25 +2,22 @@ import { NextRequest, NextResponse } from 'next/server';
 import { query } from '../../../lib/db'; // Adjust the path as necessary
 
 export async function POST(req: NextRequest) {
-  const { userId } = await req.json();
+  const { userid, farmName, region, farmSize, mainProduction } = await req.json();
 
   try {
-    // Check if the user has a farm associated with their userId
-    const getFarmInfoQuery = `
-      SELECT 1 FROM farmInfo
-      WHERE userId = $1
+    const insertFarmQuery = `
+      INSERT INTO farmInfo (userid, farmName, region, farmSize, mainProduction, creationDate)
+      VALUES ($1, $2, $3, $4, $5, NOW())
+      RETURNING farmId
     `;
-    const result = await query(getFarmInfoQuery, [userId]);
+    const result = await query(insertFarmQuery, [userid, farmName, region, farmSize, mainProduction]);
 
-    // Check if `result.rowCount` is not null
-    const farmExists =  result.rowCount && result.rowCount > 0;
-    
-    // Return true if a farm exists, otherwise false
-    return NextResponse.json({ farmExists }, { status: 200 });
+    const newFarmId = result.rows[0].farmid;
+
+    return NextResponse.json({ message: 'Farm info saved', farmId: newFarmId }, { status: 200 });
   } catch (error) {
-    console.error('Error checking farm info:', error);
+    console.error('Error saving farm info:', error);
     return NextResponse.json({ error: 'Internal Server Error' }, { status: 500 });
   }
 }
-
 
